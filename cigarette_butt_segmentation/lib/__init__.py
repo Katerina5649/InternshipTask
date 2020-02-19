921be3f1fa62c2b8b04558961f3205c39abb1530
Kate Trimbach <katetrimbach@Kates-MacBook-Air.local>
2020-02-19 23:20:48 +0300

    Add models, notebooks

---
+++ cigarette_butt_segmentation/lib/__init__.py
@@ -0,0 +1,7 @@
+from lib.metrics import get_dice
+from lib.dice_loss import dice_coeff
+from lib.utils import encode_rle, decode_rle, get_mask
+from lib.show import show_img_with_mask
+from lib.html import get_html
+from lib.eval import eval_net
+from lib.ut import get_mask
